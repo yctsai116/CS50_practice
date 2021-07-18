95e9794e711e3fd93f56c95c3a585ff0b43335f3
YC Tsai <yctsai116@gmail.com>
2021-07-18 15:17:52 +0800

    flight and airport with admin

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -2,9 +2,21 @@ from django.db import models
 
 # Create your models here.
 #  Every model is going to be a python class.
+
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.city} ({self.code})"
+
 class Flight(models.Model):
-    origin = models.CharField(max_length=64)
-    destination = models.CharField(max_length=64)
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
+    # on_delete -> if we delete the airport(ex:JFK) from airports table, we also delete the flight
+    # we can also prevent deleting airport if any flight fly from or to the airport (models.protect)
+    # related name -> a way of me accessing a relationship in a reverse order
+    # ex: I want to see all flights have that airport as an origin.
     duration = models.IntegerField()
 
     def __str__(self):
